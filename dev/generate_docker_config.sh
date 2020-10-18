9e2b75f84af8217c6cf72c69aef0dadce139e2b4
Trevor Mc <sweeney.trevor94@gmail.com>
2020-10-18 20:55:36 +0100

    Merging Footer Update

--- dev/generate_docker_config.sh
+++ dev/generate_docker_config.sh
@@ -6,7 +6,7 @@ sed -i -e '/STATIC_ROOT/d' /config/docker_config.py
 echo "SECRET_KEY = \"${SECRET_KEY}\"" >> /config/docker_config.py
 echo "STATIC_ROOT = \"/static/\"" >> /config/docker_config.py
 echo "DEBUG = True" >> /config/docker_config.py
-echo "ALLOWED_HOSTS.append('0.0.0.0')" >> /config/docker_config.py
+echo "ALLOWED_HOSTS.append('172.25.157.66')" >> /config/docker_config.py
 echo "EMAIL_HOST = 'smtp'" >> /config/docker_config.py
 echo "EMAIL_PORT = 2525" >> /config/docker_config.py
 echo "MEDIA_ROOT = '/mediaroot/'" >> /config/docker_config.py
